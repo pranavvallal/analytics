0a7ad6a4e68a033d7df523ba4ac5652def50a25e
pranavvallal <vallalpranav@gmail.com>
2018-04-29 13:47:36 +0530

    day4

---
+++ Date.R
@@ -0,0 +1,109 @@
+#Handling and creating Dates in R
+
+#date in yyyy-mm-dd
+dates1a = c('2018-21-4', '2018-29-4')
+class(dates1a)
+?as.Date
+as.Date(dates1a)  # error
+dates1a
+dates1b = as.Date(dates1a, format='%Y-%d-%m')
+class(dates1b)
+dates1b
+dates1b + 1 + 365 + 365
+
+as.Date('2019-02-28') +1 
+
+#default format %Y-%m-%d
+dates2a = c('2018-04-21')
+class(dates2a)  # character so far
+(dates2b = as.Date(dates2a))  # convert to date
+class(dates2b)  # date now
+format(dates2b, format=('%d-%m-%Y')) # print in different format
+
+format(dates2b, format=('%d-%B-%y'))
+format(dates2b, format=('%A-%B-%Y'))
+
+#Format Types ----
+#%Y: 4-digit year (1982),%y: 2-digit year (82)
+#%m: 2-digit month (01), %d: 2-digit day of the month (13)
+#%A: weekday (Wednesday), %a: abbreviated weekday (Wed)
+#%B: month (January), %b: abbreviated month (Jan)
+
+format(dates2b, format="%A  %d  %m  %y") # another format
+format(as.date(21-04-2018,'%d-%m-%Y'),'%A')
+
+
+
+#System Date
+Sys.Date()
+format(Sys.Date(), format="%A %d %B")
+
+#increment/ decrement dates 
+dates2b
+(course = dates2b + 0:11) # start : 11 days course
+
+cat(format(course, format="%B-%d"))
+months(course)
+weekdays(course)
+quarters(course)
+paste(quarters(course),'2018',sep = '--')
+#Date to character
+dates2b; class(dates2b)
+(dtoc = as.character(dates2b))
+class(dtoc)
+
+#one more practise on date format
+dates3a =as.Date('30Apr18',format("%d%b%y"))
+class(dates3a)
+
+#Difference in dates
+course
+min(course)  # first date
+max(course)  # last date
+range(course) # start to end
+mean(course)  # avg date ?
+median(course) # middle date
+course[c(1,6)] # 1st(not 0th) & 5th date 
+course[1] ; course[11]
+(duration = course[8] - course[1])
+(duration2 = max(course) - min(course))
+dob= as.Date('06-10-1994','%d-%m-%Y')
+dob
+dob1 = dob + 1:1000
+dob1
+Sys.Date()
+Sys.Date() - dob
+as.numeric((Sys.Date() - dob)/365)
+boxplot((as.numeric(Sys.Date() - dob1)/365))
+mean(as.numeric(Sys.Date() - dob1)/365)
+#Sequence of Dates'
+?seq
+
+#seq(from, to, by, length.out = NULL, along.with = NULL, ...)
+#by years : from start date to end date
+seq(as.Date("2016/1/1"), as.Date("2018/5/1"), "years")
+seq(as.Date("2016/1/1"), as.Date("2018/5/1"), "")
+
+## by month : 6 months
+seq(as.Date("2017/1/1"), by = "month", length.out = 6)
+seq(as.Date("2017/1/1"), by = "month", along.with = c(3,2,43))  # 
+seq(as.Date("2017/1/1"), by = "month", length.out = length(c(3,2,43)))
+seq(as.Date("2017/1/1"), by = "quarter", along.with = c(3,2,43))  # 
+
+
+
+## quarters
+seq(as.Date("2017/1/1"), as.Date("2018/1/1"), by = "quarter")
+#Find 7th of all months between two dates
+(dateseqF = seq(as.Date("2017/1/7"), as.Date("2018/1/7"), by = "1 month"))
+format(dateseqF, format='%A-%d-%B' )# order
+# dates in opposite order
+(dateseq = seq(as.Date("2018/1/7"), as.Date("2017/1/7"), by = "-1 month"))  # reverse order
+# then reverse them
+rev(dateseq)# correct the order
+format(rev(dateseq), "%A : %d-%b")
+
+#another Sequence : using another package
+chron::seq.dates("01/01/2017", "12/31/2017", by = "months")
+chron::seq.dates("01/01/2017", "12/31/2017", by = "weeks")
+
---
+++ Simple moving average.R
@@ -0,0 +1,49 @@
+# TS - Simple Moving Average
+#SMA
+# use for 
+#Data settechnique to get an overall idea of the trends in a data set; it is an average of any subset of numbers. The moving average is extremely useful for forecasting long-term trends
+
+
+Nile  # river flow from year 1871 to 1970
+
+plot(Nile)
+
+library(forecast)  # for forecasting
+opar = par(no.readonly = T) #save current parameters
+par(mfrow=c(2,2))
+(ylim = range(Nile))
+plot(Nile, main='Original TS')
+head(Nile)
+ma(Nile,3)
+head(Nile,4)
+head(ma(Nile,3))
+(1120+1160+963)/3
+mean(Nile[1:3]);mean(Nile[2:4]); mean(Nile[3:5]); mean(Nile[4:6])
+ma(Nile,3)
+
+plot(ma(Nile,3), main='SMA k=3', ylim=ylim)
+plot(ma(Nile,7), main='SMA k=7', ylim=ylim)
+plot(ma(Nile,15),main='SMA k=15', ylim=ylim)
+par(opar)
+
+#mfrow mfcol
+mtcars
+names(mtcars)
+plot(mtcars$mpg)      
+par(mfrow=c(1,2))
+plot(mtcars$mpg)
+plot(mtcars)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
